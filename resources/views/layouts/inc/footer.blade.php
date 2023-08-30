4bb0d5cbede7fddac5968aee13d1afa415f8652b
Plaifah <praypar5689@gmail.com>
2023-08-30 15:33:49 +0700

    first commit

--- resources/views/layouts/inc/footer.blade.php
+++ resources/views/layouts/inc/footer.blade.php
@@ -2,9 +2,9 @@
   <footer class="main-footer">
     <!-- To the right -->
     <div class="float-right d-none d-sm-inline">
-        Anything you want
+        Vertion 1-2566
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    <strong>Copyright &copy; 2014-2031 <a href="https://www.trattc.ac.th">PLaifah</a>.</strong> All rights
     reserved.
 </footer>
